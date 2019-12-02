d82ddf08b9dca622860e3ae9c78f6370637162bf
Stanisław Lutkiewicz <s17535@pjwstk.edu.pl>
2019-12-02 20:02:49 +0100

    comments added + meta scheme

--- routes/projects.js
+++ routes/projects.js
@@ -1,32 +1,45 @@
 var express = require('express');
 var router = express.Router();
-var template_ = {
+var projectTemplate_ = {
     id : 14,
     userId : "",
     name : "",
     primaryMethod : "POMODORO",
     projectStatus : "ACTIVE",
     statsTemplate : 1,
+    projectUsageIds : [1,2],
     "status" : 0
 }
+var projectUsageTemplate_ = {
+    id : 1,
+    projectId : 14,
+    method : 14,
+    timeSpend : 25,
+    "status" : 0
+}
+//STARTING PATH {url}/projects
+
 /* GET Projects {id}||{Username}||{UsernameId}. */
 router.get('/', function(req, res, next) {
     
     var projectId =  req.query.projectId;
     var userName = req.query.userName;
     var usernameId = req.query.usernameId;
-    var response = template_;
-    console.log(projectId,userName,usernameId);
+    var response = projectTemplate_;
     
         if (typeof projectId != 'undefined'){
+            //TODO query
             response["id"] = projectId;
             res.status(200).send(response);
         }
+        
         else if (typeof usernameId != 'undefined') {
+            //TODO query
             response["userId"] = usernameId;
             res.status(200).send(response);
         }
         else if (typeof userName != 'undefined'){
+            //TODO query
             res.status(200).send(response);
         }
         else { 
@@ -37,20 +50,18 @@ router.get('/', function(req, res, next) {
             res.status(200).send(response);
         }
 });
+
+/* POST Projects json{{projectData}}. */
 router.post('/', function(req, res, next) {
     
     var name = req.body.name;
     var usernameId = req.body.userId;
-
-    console.log(usernameId)
-    console.log(req.body.name)
         var valid = 'true';
         if(typeof usernameId == 'undefined' ){
-            
             valid = 'false'
         }    
         if (valid == 'true'){
-            response = template_;
+            response = projectTemplate_;
             response["userId"]=usernameId;
             response["name"]=name;
             //TODO save data
@@ -64,17 +75,18 @@ router.post('/', function(req, res, next) {
             res.status(200).send(response);
         }
 });
+
+/* PUT Projects json{{id + projectData to update}}. */
 router.put('/', function(req, res, next) {
     
     var projectId =  req.body.id;
 
         var valid = 'true';
         if(typeof projectId == 'undefined' ){
-            
             valid = 'false'
         }    
         if (valid == 'true'){
-            response = template_   
+            response = projectTemplate_   
             response["id"]=projectId;
             response["name"]=req.body.name;
             response["primaryMethod"]=req.body.primaryMethod;
