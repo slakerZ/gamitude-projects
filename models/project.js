abf273b171d8df4c5fed50dfa51921e105a3a0fb
Stanisław Lutkiewicz <stas.lutkiewicz@gmail.com>
2020-01-09 22:12:55 +0100

    added enum validation & proper REST api

--- models/project.js
+++ models/project.js
@@ -1,16 +1,28 @@
 const mongoose = require("mongoose");
-const ObjectId = mongoose.Schema.Types.ObjectId; 
+const ObjectId = mongoose.Schema.Types.ObjectId;
 const projectSchema = new mongoose.Schema({
-    id: ObjectId,
-    username: { type: String },
-    userId: { type: Number },//Temporary ?? ObjectId, ref: 'User'
-    name: { type: String },
-    primaryMethod: { type: String },
-    projectStatus: { type: String },//Temporary ?? Number 
-    statsTemplate: { type: Number },//Temporary ?? document in Db
-    projectUsages: [
-        {type: mongoose.Schema.Types.ObjectId, ref: 'ProjectUsage'}
-      ]
+  id: ObjectId,
+  userId: {
+    type: String,
+    required: true
+  },
+  name: { type: String },
+  primaryMethod: {
+    type: String,
+    enum: ["POMODORO", "90/30"]
+  },
+  projectStatus: {
+    type: String,
+    enum: ["ACTIVE", "ONHOLD", "DONE"]
+  },
+  statsTemplate: { type: Number },//Temporary ?? document in Db || just {stat1:value ....}
+  projectUsages: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'ProjectUsage' }
+  ]
 });
 const Project = mongoose.model("Project", projectSchema);
 module.exports = Project;
+
+
+
+
