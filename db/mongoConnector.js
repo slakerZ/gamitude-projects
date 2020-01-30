f342eedbb7a48a3983f8377fb984cc8702bb1c30
slutkiewicz <33038766+slutkiewicz@users.noreply.github.com>
2020-01-30 14:03:15 +0100

    mongo connection update + projectUsage catch block (#12)

--- db/mongoConnector.js
+++ db/mongoConnector.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDb = () => {
   return mongoose.connect(process.env.DATABASE_URL || require("./connectionString.json")["mongoConnStringDev"], {
-     useNewUrlParser: true })
+     useNewUrlParser: true,useUnifiedTopology: true })
 };
 
 module.exports = connectDb;
