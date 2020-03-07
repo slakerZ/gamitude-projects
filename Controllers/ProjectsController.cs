cba19c914adf16b780fee690dde7bd3802555fab
Stanislaw Lutkiewicz <stas.lutkiewicz@gmail.com>
2020-03-07 21:42:14 +0100

    Proper auth all working

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -6,23 +6,28 @@ using Microsoft.AspNetCore.Authorization;
 using System.Net.Http.Headers;
 using Microsoft.Extensions.Primitives;
 using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace ProjectsApi.Controllers
 {
     [Route("api/[controller]")]
-    // [Authorize]
+    [Authorize]
     [ApiController]
     public class ProjectsController : ControllerBase
     {
-        private readonly String tokenName = "x-api-token";
+        //TODO stats verification if Dominant in stats
 
         private readonly ProjectService _projectService;
         private readonly UserTokenService _userTokenService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public ProjectsController(ProjectService projectService, UserTokenService userTokenService)
+        public ProjectsController(ProjectService projectService, UserTokenService userTokenService, IHttpContextAccessor httpContextAccessor)
         {
             _projectService = projectService;
             _userTokenService = userTokenService;
+            _httpContextAccessor = httpContextAccessor;
 
         }
 
@@ -30,113 +35,134 @@ namespace ProjectsApi.Controllers
         public ActionResult<List<Project>> Get()
         {
 
-            StringValues token;
+            string userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name).ToString();
 
-            if (Request.Headers.TryGetValue(tokenName, out token))
+            if (null != userId)
             {
-                string userId = _userTokenService.GetClaim(token.ToString());
-                if (null != userId)
-                {
-                    return _projectService.GetProjectsByUserId(userId);
-                }
+                return _projectService.GetProjectsByUserId(userId);
+            }
+            else
+            {
+                return NotFound();
             }
 
-            return Unauthorized();
 
         }
 
         [HttpGet("{id:length(24)}", Name = "GetProject")]
         public ActionResult<Project> Get(string id)
         {
-            StringValues token;
 
-            if (Request.Headers.TryGetValue(tokenName, out token))
+            string userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name).ToString();
+
+            if (null != userId)
             {
-                string userId = _userTokenService.GetClaim(token.ToString());
-                if (null != userId)
-                {
-                    var project = _projectService.Get(id);
+                return _projectService.Get(id);
 
-                    if (project == null)
-                    {
-                        return NotFound();
-                    }
+            }
+            else
+            {
+                return NotFound("User Failure");
 
-                    return project;
-                }
             }
 
-            return Unauthorized();
         }
 
         [HttpPost]
         public ActionResult<Project> Create(Project project)
         {
-            StringValues token;
+            string userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name).ToString();
 
-            if (Request.Headers.TryGetValue(tokenName, out token))
+
+            if (null != userId)
             {
-                string userId = _userTokenService.GetClaim(token.ToString());
+                project.UserId = userId;
+                project.DateAdded = DateTime.UtcNow;
+                _projectService.Create(project);
 
-                if (null != userId)
-                {
-                    project.UserId = userId;
-                    project.DateAdded = DateTime.UtcNow;
-                    _projectService.Create(project);
-
-                    return CreatedAtRoute("GetProject", new { id = project.Id.ToString() }, project);
-                }
+                return CreatedAtRoute("GetProject", new { id = project.Id.ToString() }, project);
             }
-            return Unauthorized();
+            else
+            {
+                return NotFound("User Failure");
+
+            }
+
+
         }
 
 
         [HttpPut("{id:length(24)}")]
         public IActionResult Update(string id, Project projectIn)
         {
-            StringValues token;
+            string userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name).ToString();
 
-            if (Request.Headers.TryGetValue(tokenName, out token))
+            if (null != userId)
             {
-                string userId = _userTokenService.GetClaim(token.ToString());
-                if (null != userId)
+                var project = _projectService.Get(id);
+                if (project == null)
                 {
-                    var project = _projectService.Get(id);
-
-                    if (project == null)
-                    {
-                        return NotFound();
-                    }
-
-                    _projectService.Update(id, projectIn);
+                    return NotFound("Project not found");
                 }
+                projectIn = updateProject(project, projectIn);
+
+                _projectService.Update(id, projectIn);
+                return Ok();
             }
-            return Unauthorized();
+            else
+            {
+                return NotFound("User Failure");
+
+            }
         }
+
 
         [HttpDelete("{id:length(24)}")]
         public IActionResult Delete(string id)
         {
-            StringValues token;
+            string userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name).ToString();
 
-            if (Request.Headers.TryGetValue(tokenName, out token))
+            if (null != userId)
             {
-                string userId = _userTokenService.GetClaim(token.ToString());
-                if (null != userId)
+
+                var project = _projectService.Get(id);
+
+                if (project == null)
                 {
-
-                    var project = _projectService.Get(id);
-
-                    if (project == null)
-                    {
-                        return NotFound();
-                    }
-
-                    _projectService.Remove(project.Id);
-
+                    return NotFound();
                 }
+
+                _projectService.Remove(project.Id);
+                return Ok();
+
             }
-            return Unauthorized();
+            else
+            {
+                return NotFound("User Failure");
+
+            }
+
+        }
+        private Project updateProject(Project project, Project projectIn)
+        {
+            projectIn.Id=project.Id;
+            if (null == projectIn.Name)
+            {
+                projectIn.Name = project.Name;
+            }
+            if (null == projectIn.PrimaryMethod.ToString())
+            {
+                projectIn.PrimaryMethod = project.PrimaryMethod;
+            }
+            if (null == projectIn.ProjectStatus.ToString())
+            {
+                projectIn.ProjectStatus = project.ProjectStatus;
+            }
+            if (null == projectIn.Stats)
+            {
+                projectIn.Stats = project.Stats;
+            }
+            return projectIn;
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -39,28 +39,28 @@ namespace gamitude_projects
             // Console.WriteLine(value);
             var appSettingsSection = Configuration.GetSection("AppSettings");
             services.Configure<AppSettings>(appSettingsSection);
-
-            // //AUTHENTICATION
-            // // configure jwt authentication
-            // var appSettings = appSettingsSection.Get<AppSettings>();
-            // var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-            // services.AddAuthentication(x =>
-            // {
-            //     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //     x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            // })
-            // .AddJwtBearer(x =>
-            // {
-            //     x.RequireHttpsMetadata = false;
-            //     // x.SaveToken = true;
-            //     x.TokenValidationParameters = new TokenValidationParameters
-            //     {
-            //         ValidateIssuerSigningKey = true,
-            //         IssuerSigningKey = new SymmetricSecurityKey(key),
-            //         ValidateIssuer = false,
-            //         ValidateAudience = false
-            //     };
-            // });
+            services.AddHttpContextAccessor();
+            //AUTHENTICATION
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = false;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
             //DATABASE SETTINGS
             services.Configure<ProjectsDatabaseSettings>(
@@ -88,9 +88,9 @@ namespace gamitude_projects
 
             // app.UseHttpsRedirection();
 
-            // app.UseAuthentication();
-            // app.UseAuthorization();
+            app.UseAuthentication();
             app.UseRouting();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
