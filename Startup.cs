6e4a7b319cc100cad09094046f0a3010774d9e0c
Stanislaw Lutkiewicz <stas.lutkiewicz@gmail.com>
2020-03-24 20:08:47 +0100

    postman collection added

--- Startup.cs
+++ Startup.cs
@@ -35,11 +35,12 @@ namespace gamitude_projects
         {
             Console.WriteLine("starting");
             //ADDITIONAL SETTINGS
-            // var value = System.Environment.GetEnvironmentVariable("");
-            // Console.WriteLine(value);
+
             var appSettingsSection = Configuration.GetSection("AppSettings");
+
             services.Configure<AppSettings>(appSettingsSection);
             services.AddHttpContextAccessor();
+
             //AUTHENTICATION
             // configure jwt authentication
             var appSettings = appSettingsSection.Get<AppSettings>();
@@ -70,7 +71,7 @@ namespace gamitude_projects
 
             //DATABASE SERVICES
             //MAYBE SCOPED??
-            services.AddSingleton<ProjectService>();
+            services.AddScoped<ProjectService>();
 
             services.AddControllers().AddJsonOptions(opt =>
                 {
